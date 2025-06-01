b32cc3a6a1b9915b4306556dd5424047fa7f7c8e
MaxtuneLee <max@xox.im>
2025-06-01 14:35:34 +0800

    fix: update site URLs and original image links to use new asset domain

--- config/site.config.ts
+++ config/site.config.ts
@@ -2,14 +2,14 @@ export const siteConfig = {
   name: "Maxtune's Photo Gallery",
   title: 'Maxtune Photography',
   description: '人生路漫漫，白鹭常相伴',
-  url: process.env.SITE_URL || 'https://gallery.mxte.cc',
+  url: 'https://gallery.mxte.cc',
   ogImage: {
     width: 1200,
     height: 630,
   },
   author: {
     name: 'Maxtune Photography',
-    url: process.env.SITE_URL || 'https://mxte.cc/',
+    url: 'https://mxte.cc/',
   },
   social: {
     twitter: '@MaxtuneLee',
